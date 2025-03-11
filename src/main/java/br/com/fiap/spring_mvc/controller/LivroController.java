274d00071c51c5489c339e0659c80e5dd5ef4f75
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-03-11 09:49:46 -0300

    :sparkles: feat: internacionalizacao e renderizando os erros na tela

--- src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
+++ src/main/java/br/com/fiap/spring_mvc/controller/LivroController.java
@@ -6,56 +6,61 @@ import br.com.fiap.spring_mvc.service.LivroService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-
 
 @Controller
 @RequestMapping("/livro")
 public class LivroController {
     @Autowired
     LivroService livroService;
+
     @GetMapping("/cadastro")
-    public ModelAndView livroCadastro(){
-        ModelAndView mv = new ModelAndView("livroCadastro");
-        mv.addObject("livroRequest",new LivroRequest());
-        return mv;
+    public String livroCadastro(Model model) {
+        model.addAttribute("livroRequest", new LivroRequest());
+        return "livroCadastro";
     }
+
     @PostMapping("/cadastrar")
-    public ModelAndView cadastrarLivro(@Valid @ModelAttribute LivroRequest livroRequest){
-        Livro livro = livroService.salvarLivro(livroRequest);
-        return listarLivros();
-    }
-    @GetMapping("/lista")
-    public ModelAndView listarLivros(){
-        ModelAndView mv = new ModelAndView("livroLista");
-        List<Livro> livros = livroService.buscarLivros();
-        mv.addObject("listaLivros", livros);
-        return mv;
-    }
-    @GetMapping("/edicao/{id}")
-    public ModelAndView livroEdicao(@PathVariable Long id){
-        Livro livro = livroService.buscarLivro(id);
-        if(livro == null){
-            return listarLivros();
+    public String cadastrarLivro(@Valid @ModelAttribute LivroRequest livroRequest, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            return "livroCadastro";
         }
-        ModelAndView mv = new ModelAndView("livroEdicao");
-        mv.addObject("idLivro", id);
-        mv.addObject("livro", livroService.livroToRequest(livro));
-        return mv;
+        livroService.salvarLivro(livroRequest);
+        return listarLivros(model);
     }
+
+    @GetMapping("/lista")
+    public String listarLivros(Model model){
+        List<Livro> livros = livroService.buscarLivros();
+        model.addAttribute("listaLivros", livros);
+        return"livroLista";
+    }
+
+    @GetMapping("/edicao/{id}")
+    public String livroEdicao(@PathVariable Long id, Model model){
+        Livro livro = livroService.buscarLivro(id);
+        if (livro == null) {
+            return listarLivros(model);
+        }
+        model.addAttribute("idLivro", id);
+        model.addAttribute("livro", livroService.livroToRequest(livro));
+        return "livroEdicao";
+    }
+
     @PostMapping("/editar/{id}")
-    public ModelAndView editarLivro(@PathVariable Long id, @Valid @ModelAttribute LivroRequest livroRequest){
+    public String editarLivro(@PathVariable Long id, @Valid @ModelAttribute LivroRequest livroRequest, Model model){
         livroService.atualizarLivro(id, livroRequest);
-        return listarLivros();
+        return listarLivros(model);
     }
 
     @GetMapping("/deletar/{id}")
-    public ModelAndView deletarLivro(@PathVariable Long id){
+    public String deletarLivro(@PathVariable Long id, Model model){
         livroService.deletarLivro(id);
-        return listarLivros();
+        return listarLivros(model);
     }
-
 }
+
